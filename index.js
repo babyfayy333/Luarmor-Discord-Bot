3368df8c0bccfbd53ae574e6d195ffeee277cf0c
Babyfayy333 <fayy@farm-hub.tech>
2024-06-18 10:12:51 +1200

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { discord_token } = require('./config.json');
 
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, bot => {
+	console.log(`[Ready]: Logged in as bot, ${bot.user.tag}!`);
+});
+
+client.login(discord_token);
