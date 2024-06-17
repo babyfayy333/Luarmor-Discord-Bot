3511d77bca35d5b8b64a9492cd565b98cd949d10
Babyfayy333 <fayy@farm-hub.tech>
2024-06-18 10:09:26 +1200

    Update luarmor.js

--- modules/luarmor.js
+++ modules/luarmor.js
@@ -1,9 +1,5 @@
-// Luarmor api by @babyfayy333 on github made with fetch
-// Luarmor API key & whitelisted server ip required
-// Refrences: https://docs.luarmor.net/
-
 const fetch = require('node-fetch');
-const base_url = "https://api.luarmor.net";
+const base_url = 'https://api.luarmor.net';
 
 // API KEY MANAGEMENT 
 
@@ -25,20 +21,20 @@ async function get_key_stats(api_key) {
 // USER & KEY MANAGEMENT
 
 async function create_user(api_key, project_id, data) {
-    if (!data) throw new Error("Data is required");
+    if (!data) throw new Error('Data is required');
 
     const user_data = {
         identifier: data.identifier || null,
         key_days: data.granted_days || null, // null = never expire
-        note: data.user_note || "Not provided",
+        note: data.user_note || 'Not provided',
         discord_id: data.discord_id || null,
     }
 
     const response = await fetch(`${base_url}/v3/projects/${project_id}/users`, {
-        method: "POST",
+        method: 'POST',
         headers: {
-            "Authorization": `${api_key}`,
-            "Content-Type": "application/json"
+            'Authorization': `${api_key}`,
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(user_data)
     });
@@ -47,20 +43,20 @@ async function create_user(api_key, project_id, data) {
 }
 
 async function update_user(api_key, project, data) {
-    if (!data) throw new Error("Data is required");
+    if (!data) throw new Error('Data is required');
 
     const user_data = {
         key_days: data.granted_days || null, // null = never expire
-        note: data.user_note || "Not provided",
+        note: data.user_note || 'Not provided',
         discord_id: data.discord_id || null,
         user_key: data.user_key || null
     }
 
     const response = await fetch(`${base_url}/v3/projects/${project_id}/users/${user_id}`, {
-        method: "PATCH",
+        method: 'PATCH',
         headers: {
-            "Authorization": `${api_key}`,
-            "Content-Type": "application/json"
+            'Authorization': `${api_key}`,
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(user_data)
     });
@@ -70,10 +66,10 @@ async function update_user(api_key, project, data) {
 
 async function delete_user(api_key, project_id, script_key) {
     const response = await fetch(`${base_url}/v3/projects/${project_id}/users?user_key=${script_key}`, {
-        method: "DELETE",
+        method: 'DELETE',
         headers: {
-            "Authorization": `${api_key}`,
-            "Content-Type": "application/json"
+            'Authorization': `${api_key}`,
+            'Content-Type': 'application/json'
         }
     });
 
@@ -83,8 +79,8 @@ async function delete_user(api_key, project_id, script_key) {
 async function get_users(api_key, project_id) {
     const response = await fetch(`${base_url}/v3/projects/${project_id}/users`, {
         headers: {
-            "Authorization": `${api_key}`,
-            "Content-Type": "application/json"
+            'Authorization': `${api_key}`,
+            'Content-Type': 'application/json'
         }
     });
 
@@ -94,8 +90,8 @@ async function get_users(api_key, project_id) {
 async function get_user(api_key, project_id, discord_id) {
     const response = await fetch(`${base_url}/v3/projects/${project_id}/users?discord_id=${discord_id}`, {
         headers: {
-            "Authorization": `${api_key}`,
-            "Content-Type": "application/json"
+            'Authorization': `${api_key}`,
+            'Content-Type': 'application/json'
         }
     });
 
@@ -106,14 +102,14 @@ async function reset_hwid(api_key, project_id, discord_id, forced) {
     // get script_key from get_user
 
     const user = get_user(api_key, project_id, discord_id);
-    if (!user.users[0]) throw new Error("User not found");
+    if (!user.users[0]) throw new Error('User not found');
     const user_data = user.users[0];    
 
     const response = await fetch(`${base_url}/v3/projects/${project_id}/users/resethwid`, {
-        method: "POST",
+        method: 'POST',
         headers: {
-            "Authorization": `${api_key}`,
-            "Content-Type": "application/json"
+            'Authorization': `${api_key}`,
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify({user_key: user_data.script_key, force: forced})
     });
@@ -121,10 +117,10 @@ async function reset_hwid(api_key, project_id, discord_id, forced) {
 
 async function link_discord(api_key, project, script_key, discord_id, forced) {
     const response = await fetch(`${base_url}/v3/projects/${project_id}/users/linkdiscord`, {
-        method: "POST",
+        method: 'POST',
         headers: {
-            "Authorization": `${api_key}`,
-            "Content-Type": "application/json"
+            'Authorization': `${api_key}`,
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify({user_key: script_key, discord_id: discord_id, force: forced})
     });
@@ -132,15 +128,14 @@ async function link_discord(api_key, project, script_key, discord_id, forced) {
 
 async function blacklist_user(api_key, project_id, script_key, reason) {
     const response = await fetch(`${base_url}/v3/projects/${project_id}/users/blacklist`, {
-        method: "PATCH",
+        method: 'PATCH',
         headers: {
-            "Authorization": `${api_key}`,
-            "Content-Type": "application/json"
+            'Authorization': `${api_key}`,
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify({user_key: script_key, reason: reason})
     });
 }
-
 
 // EXPORT CUSTOM FUNCTIONS
 
